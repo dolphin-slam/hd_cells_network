2a9a8b910ad13893a9cff7544a860f058046dea5
Luan <luansilveira.dp@gmail.com>
2014-06-24 10:18:10 -0300

    Correção das funções de plotagem da rede. STATUS: Primeiros testes rodados. TODO: Testar com dados sintéticos de imu e com menos neurônios.

--- src/hd_cells_network_node.cpp
+++ src/hd_cells_network_node.cpp
@@ -5,14 +5,20 @@
 #include "visualization_msgs/Marker.h"
 #include <tf/tf.h>
 #include <angles/angles.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 using namespace hd_cells;
 
-const double MARKER_SCALE = 0.25;
+const double MARKER_SCALE = 0.1;
 const int NUMBER_OF_NEURONS = 100;
 const double STD_IMU = angles::from_degrees(10);
 
 HDCellsNetwork network(NUMBER_OF_NEURONS);
+
+ros::Publisher marker_publisher;
 
 
 void publishNetworkActivity()
@@ -34,9 +40,11 @@ void publishNetworkActivity()
     message.id = 0;
 
     //! configura a pose dos marcadores
-    message.pose.position.x = -activity.size()/2;
+    message.pose.position.x = -5; //-activity.size()*MARKER_SCALE/2;
     message.pose.position.y = 0;
     message.pose.position.z = 0;
+
+    //cout << activity.size() << " " << MARKER_SCALE << " " << -activity.size()*MARKER_SCALE/2 << endl ;
 
     message.pose.orientation.x = 0.0;
     message.pose.orientation.y = 0.0;
@@ -59,10 +67,13 @@ void publishNetworkActivity()
     for(int i=0;i<activity.size();i++)
     {
         //! set point position
-        message.points[i].x = i;
-        message.points[i].y = 0;
-        message.points[i].z = activity[i];
+        message.points[i].x = i*MARKER_SCALE;
+        message.points[i].y = activity[i];
+        message.points[i].z = 0;
     }
+
+
+    marker_publisher.publish(message);
 }
 
 void publishNetworkInput(std::vector<double> input)
@@ -81,9 +92,9 @@ void publishNetworkInput(std::vector<double> input)
     message.id = 0;
 
     //! configura a pose dos marcadores
-    message.pose.position.x = -input.size()/2;
-    message.pose.position.y = 1;
-    message.pose.position.z = 0;
+    message.pose.position.x = -5;
+    message.pose.position.y = 3;
+    message.pose.position.z = 1;
 
     message.pose.orientation.x = 0.0;
     message.pose.orientation.y = 0.0;
@@ -106,10 +117,12 @@ void publishNetworkInput(std::vector<double> input)
     for(int i=0;i<input.size();i++)
     {
         //! set point position
-        message.points[i].x = i;
-        message.points[i].y = 0;
-        message.points[i].z = input[i];
+        message.points[i].x = i*MARKER_SCALE;
+        message.points[i].y = input[i];
+        message.points[i].z = 0;
     }
+
+    marker_publisher.publish(message);
 }
 
 
@@ -141,7 +154,7 @@ void timerCallback(const ros::TimerEvent& event)
 
     std::vector <double> input;
     network.getLastInput(input);
-    publishNetworkInput(input);;
+    publishNetworkInput(input);
 
 }
 
@@ -150,7 +163,7 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "hd_cells_network");
 
-    network.initWeights(10*M_PI/180,GAUSSIAN);
+    network.initWeights(angles::from_degrees(10),GAUSSIAN);
     network.setGlobalInhibition(0.001);
 
     //! ROS Node Handle
@@ -158,9 +171,9 @@ int main(int argc, char **argv)
 
     //! ROS Topics
     //ros::Subscriber imu_subscriber_ = node_handle_.subscribe("/imu",1,&imuCallback);
+    marker_publisher = node_handle_.advertise<visualization_msgs::Marker>("network",1);
 
     ros::Timer timer = node_handle_.createTimer(ros::Duration(1), &timerCallback);
-
 
     ros::spin();
 
